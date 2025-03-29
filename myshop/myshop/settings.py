3b6caebed1d9bb3b55a3c8e2f49d5fa649f9249d
Vladimir Molchanov <r1ken@mail.ru>
2025-03-29 10:50:18 +0300

    Add app cart, tamplate, views

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
